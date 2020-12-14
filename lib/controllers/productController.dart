e570ab00943f62d201e828a58325534872ff8df5
Manish Kumar Rajak <52307777+visionxStudio@users.noreply.github.com>
2020-12-14 16:42:42 +0545

    first commit

--- lib/controllers/productController.dart
+++ lib/controllers/productController.dart
@@ -31,3 +31,28 @@ class ProductController extends GetxController {
     }
   }
 }
+
+// GetConnect
+// GetConnect is an easy way to communicate from your back to your front with http or websockets
+
+// Default configuration
+// You can simply extend GetConnect and use the GET/POST/PUT/DELETE/SOCKET methods to communicate with your Rest API or websockets.
+
+// class UserProvider extends GetConnect {
+//   // Get request
+//   Future<Response> getUser(int id) => get('http://youapi/users/$id');
+//   // Post request
+//   Future<Response> postUser(Map data) => post('http://youapi/users', body: data);
+//   // Post request with File
+//   Future<Response<CasesModel>> postCases(List<int> image) {
+//     final form = FormData({
+//       'file': MultipartFile(image, filename: 'avatar.png'),
+//       'otherFile': MultipartFile(image, filename: 'cover.png'),
+//     });
+//     return post('http://youapi/users/upload', form);
+//   }
+
+//   GetSocket userMessages() {
+//     return socket('https://yourapi/users/socket');
+//   }
+// }
